598140d26fe8801983f7464eabe01d27bee4a374
Nigel Greenway <nigel_greenway@me.com>
2015-03-26 08:59:09 +0000

    Add Command/Mediator interface

---
+++ src/CommandInterface.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of the nigelgreenway/demander package.
+ *
+ * (c) Nigel Greenway <nigel_greenway@me.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Demander;
+
+/**
+ * Interface for a Command Message
+ *
+ * @package Demander
+ * @author  Nigel Greenway <nigel_greenway@me.com>
+ */
+interface CommandInterface
+{
+}
